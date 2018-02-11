fc4bfcfb7abdb278f282e1a6f6c3be0a07a42ffc
Elanis <elanis@hotmail.com>
2018-02-11 12:16:55 +0100

    Added hamburger menu css

--- basefiles/index.php
+++ basefiles/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Dysnomia Studio</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="lib/style.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	</head>
-	<body>
 		<div id="menu-back"></div>
 		<img src="img/hamburger.png" id="hamburger">
 		<nav id="menu">
