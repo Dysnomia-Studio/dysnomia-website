e380191b84feed43e0b6eced3be680c40a3ff893
Elanis <elanis@hotmail.com>
2018-02-11 11:38:28 +0100

    Initial commit: HTML & CSS

---
+++ basefiles/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dysnomia Studio</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="lib/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div id="menu-back"></div>
+		<img src="img/hamburger.png" id="hamburger">
+		<nav id="menu">
+			<ul>
+				<li><a href="/">Dysnomia</a></li> <!-- @ADD: Logo -->
+				<li><a href="#whoami">Qui sommes nous ?</a></li>
+				<li><a href="#projects">Projets</a></li>
+				<!--<li><a href="#prestations">Prestations</a></li>-->
+				<li><a href="#contact">Contact</a></li>
+				<li><a href="#partners">Partenaires</a></li>
+			</ul>
+		</nav>
+
+		<section id="index-top" class="corps">
+			<h1>Dysnomia</h1>
+			<p>Creation & Gestion de sites et d'applications dans le web</p>
+		</section>
+
+		<div id="whoami" class="anchor"></div>
+		<section id="index-comp-desc" class="corps-text">
+			<h2>Qui sommes nous ?</h2>
+			Dysnomia est une entreprise fondée en 2018 par Axel Soupé alias Elanis, le but de cette entreprise est de créer un reseau de sites web dans differents domaines tels que le jeu-video, les outils de develloppement, le divertissement, etc Tout ces services sont crées autour d'une même idée fondatrice: rendre les contenus accessibles facilement, gratuitement et legalement à tout un chacun.<br/>
+			<br/>
+			Actuellement, l'entreprise est composé d'une seule personne, moi, Axel Soupé, mais j'espère pouvoir agrandir mon projet rapidement.<br/>
+		</section>
+
+		<div id="projects" class="anchor"></div>
+		<section id="index-projects" class="corps">
+			<h2>Nos projets</h2>
+			<div class="project-list">
+				<div class="project-item">
+					<span class="project-title"><a href="https://howtoopen.me" target="_blank">HowToOpen.me</a></span>
+					<span class="project-time">Depuis 2018</span><br/>
+					<span class="project-type">Outil</span><br/>
+					<span class="project-desc">HowToOpen.me est un site repertoriant les differents logiciels afin de visualiser, d'éditer, un fichier selon son extension.</span><br/><br/>
+					<span class="project-logo"><a href="https://howtoopen.me" target="_blank"><img src="img/icons/howtoopenme.jpg" alt="howtoopen.me"></a></span>
+					<span class="project-url"><a href="https://howtoopen.me" target="_blank">www.howtoopen.me</a></span><br/>
+				</div>
+
+				<div class="project-item">
+					<span class="project-title"><a href="https://dehash.me" target="_blank">Dehash.me</a></span>
+					<span class="project-time">Depuis 2017</span><br/>
+					<span class="project-type">Outil</span><br/>
+					<span class="project-desc">Dehash.me est un site permettant de hash (encrypter sans pouvoir decrypter), mais aussi de "dehash". Afin de retourner ces hash, il utilise un dictionnaire grandissant chaque jour, repertoriant le resultat hashé de chaque mot.</span><br/><br/>
+					<span class="project-logo"><a href="https://dehash.me"><img src="img/icons/dehashme.jpg" alt="dehash.me"></a></span>
+					<span class="project-url"><a href="https://dehash.me">www.dehash.me</a></span><br/>
+				</div>
+
+				<div class="project-item">
+					<span class="project-title"><a href="https://galactae.eu" target="_blank">Galactae: Space Age Dawn</a></span>
+					<span class="project-time">Depuis 2016</span><br/><br/>
+					<span class="project-type">Jeu navigateur & mobile</span><br/>
+					<span class="project-desc">Galactae: Space Age Dawn est un jeu de stratégie par navigateur novateur, en effet, le jeu est develloppé dans l'optique d'avoir un maximum de gameplay (facons de jouer) differentes. Vous y incarnez l'un des 3 dirigants proposés afin de faire évoluer votre nation et faire grandir votre influence dans la galaxie.</span><br/><br/>
+					<span class="project-logo"><a href="https://galactae.eu" target="_blank"><img src="img/icons/galactae.jpg" alt="galactae.eu"></a></span>
+					<span class="project-url">
+						<a href="https://galactae.eu" target="_blank">www.galactae.eu</a><br/><br/>
+						<a href="https://play.google.com/store/apps/details?id=eu.elanis.galactae" target="_blank"><img src="img/android.png" alt="android app"></a>
+						<a target="_blank"><img src="img/ios-soon.png" alt="ios app"></a>
+						<a target="_blank"><img src="img/windows-store-soon.png" alt="windows app"></a>
+					</span><br/>
+				</div>
+
+				<div class="project-item">
+					<span class="project-title"><a href="https://randomeme.xyz" target="_blank">Randomeme</a></span>
+					<span class="project-time">Depuis 2016</span><br/><br/>
+					<span class="project-type">Divertissement</span><br/>
+					<span class="project-desc">Le principe de ce site web est simple : vous donner aléatoirement un mème : une vidéo ou une image liée a une musique.<br/><u>Mais qu'est-ce qu'un mème ?</u><br/>Un mème internet (communément appelé "mème") est un anglicisme venant d'« Internet meme » utilisé pour décrire un élément ou un phénomène repris et décliné en masse sur internet. (<a href="https://fr.wikipedia.org/wiki/Mème_Internet" target="_blank">Article Wikipédia</a>).Le but d'un mème est de créer le rire ou l'amusement chez le lecteur ou spectateur.
+					</span><br/><br/>
+					<span class="project-logo"><a href="https://randomeme.xyz" target="_blank"><img src="img/icons/randomeme.jpg" alt="randomeme.xyz"></a></span>
+					<span class="project-url">
+						<a href="https://randomeme.xyz" target="_blank">www.randomeme.xyz</a><br/><br/>
+						<a href="https://play.google.com/store/apps/details?id=eu.elanis.randomeme" target="_blank"><img src="img/android.png" alt="android app"></a>
+						<a target="_blank"><img src="img/ios-soon.png" alt="ios app"></a>
+						<a target="_blank"><img src="img/windows-store-soon.png" alt="windows app"></a>
+					</span><br/>
+				</div>
+			</div>
+		</section>
+
+		<!--<div id="prestations" class="anchor"></div>
+		<section id="index-prestations" class="corps">
+			<h2>Prestations</h2>
+		</section>-->
+
+		<div id="contact" class="anchor"></div>
+		<section id="index-contact" class="corps">
+			<h2>Contact</h2>
+			
+			<form method="post" action="contact">
+				<input type="text" name="name" placeholder="Nom" required>
+				<input type="text" name="mail" placeholder="Mail" required><br/><br/>
+				<select name="objet">
+					<option disabled selected>Objet du message</option>
+					<option value="randomeme">Randomeme</option>
+					<option value="dehashme">Dehash.me</option>
+					<option value="howtoopenme">Howtoopen.me</option>
+					<option value="autre">Autre</option>
+				</select><br/><br/>
+				<textarea name="message"></textarea><br/>
+				<input type="submit" value="Envoyer">
+			</form>
+		</section>
+
+		<!--<div id="partners" class="anchor"></div>
+		<section id="index-partners" class="corps">
+			<h2>Nos Partenaires</h2>
+			
+		</section>-->
+
+		<footer>
+			<table>
+				<tr class="first-footer">
+					<td>		
+						<ul>
+							<li><h3>Galactae</h3></li>
+							<li><a href="https://galactae.eu">Site officiel</a></li>
+							<li><a href="https://galactae.eu/medias">Gallerie</a></li>
+							<li><a href="https://galactae.eu/blog">DevBlog</a></li>
+							<li><a href="https://forum.galacte.elanis.eu/">Forum</a></li>
+						</ul>
+					</td>
+
+					<td>
+						<ul>
+							<li><h3>Autres sites</h3></li>
+							<li><a href="https://dehash.me">Dehash.me</a></li>
+							<li><a href="https://howtoopen.me">HowToOpenMe</a></li>
+							<li><a href="https://randomeme.xyz">Randomeme</a></li>
+						</ul>
+					</td>
+
+					<td>
+						<ul>
+							<li><h3>Reseaux sociaux</h3></li>
+							<li><a href="redir/facebook.php">Facebook</a></li>
+							<li><a href="redir/twitter.php">Twitter</a></li>
+							<li><a href="redir/youtube.php">Youtube</a></li>
+							<li><a href="https://discord.gg/c8aARey">Discord</a></li>
+						</ul>
+					</td>
+
+					<td>
+						<ul>
+							<li><h3>Divers</h3></li>
+							<li><a href="https://elanis.eu">Elanis</a></li>
+							<li><a href="https://bugs.elanis.eu">Bug tracker</a></li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+			<table>
+				<tr class="second-footer">
+					<td>Dysnomia - Société au capital de XXXX€ - SIREN: .........<br/>
+					Copyright 2018 - Tout droits reservés</td>
+				</tr>
+			</table>
+		</footer>
+	</body>
+</html>
