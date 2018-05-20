4b2805fcedcf52c221072aad23f04203d72afaef
Elanis <elanis@hotmail.com>
2018-05-20 20:55:27 +0200

    Added english translation

---
+++ lang/en/main.php
@@ -0,0 +1,48 @@
+<?php
+// Menu
+define('MENU_WHOAMI','Who are we ?');
+define('MENU_PROJECTS','Projects');
+define('MENU_CONTACT','Contact');
+define('MENU_PARTNERS','Partners');
+
+// Index
+define('INDEX_MAIN_TEXT','Creating & Managing web applications');
+
+// Qui sommes nous ?
+define('WHOAMI_TITLE', MENU_WHOAMI);
+define('WHOAMI_TEXT', "Dysnomia is a french company created by Axel Soupé aka Elanis in 2018, the company goals are to create a website network in many fields like games, developpement tools, entertainment, ... All those services are created around a same idea: make contents easily reachable, free and legaly for everyone.<br/>
+	Now, the company consist of only one person, me, Axel Soupé but I hope rise my project fastly.<br/>");
+
+// Descriptions des projets
+define('DEHASH_DESC','Dehash.me is a website which permit to hash and dehash. In order to dehash, it use a growing dictionnary which stock every word and its hash.');
+define('GALACTAE_DESC',"Galactae: Space Age Dawn is an innovative browser strategy game, indeed, the game is made in order to have maximum gameplay choices. In the story, you are one of the 3 suggested leaders to grow your nation and make your power accepted in the whole galaxy.");
+define('HOWTOOPENME_DESC',"HowToOpen.me is a website where your can find how to open, edit or execute a file by its extension.");
+
+// Partners
+define('PARTNERS_TITLE', 'Our Partners');
+
+// Projets
+define('PROJECT_TITLE','Our projects');
+define('PROJECT_SINCE','Since');
+define('PROJECT_OUT','Out');
+define('PROJECT_TOOL','Tool');
+define('PROJECT_BROWSER_MOBILE_GAME','Browser & mobile game');
+
+// Contact
+define('CONTACT_TITLE','Contact');
+define('CONTACT_OBJ','Object of message');
+define('CONTACT_PARTNERS','Partner');
+define('CONTACT_CF_OFF_SITE','see official website');
+define('CONTACT_OTHERS','Other');
+define('CONTACT_ENVOYER','Send');
+
+// Footer
+define('FOOTER_OFF_WEBSITE','Official website');
+define('FOOTER_GALACTAE_GALERIE','Gallery');
+define('FOOTER_DEVBLOG','DevBlog');
+define('FOOTER_FORUM','Forum');
+define('FOOTER_OTHER_WEBSITES','Other websites');
+define('FOOTER_SOCIAL_NETWORK','Social networks');
+define('FOOTER_MISC','Misc');
+define('FOOTER_BUGTRACKER','Bug tracker');
+define('FOOTER_ALL_RIGHT_RESERVED','All rights reserved');
