17aa3b0a1e4aadff64565eeb6681134c160bb0eb
Elanis <elanis@hotmail.com>
2023-04-22 13:11:04 +0200

    Working and full project display

--- src/components/HomepageProject/index.jsx
+++ src/components/HomepageProject/index.jsx
@@ -15,11 +15,20 @@ export default function HomepageProject({ project }) {
 
 			<div className="homepage-project-details">
 				{!project.hasLogo && <h2 className="homepage-project-title">{project.name}</h2>}
-				{project.hasLogo && <img className="homepage-project-logo" src={`/img/${project.id}/logo.png`} alt={`${project.name} logo`} />}
+				{project.hasLogo && <a target="_blank" href={project.links[0].url}><img className="homepage-project-logo" src={`/img/${project.id}/logo.png`} alt={`${project.name} logo`} /></a>}
 
 				{project.screenshots > 0 && <img className="homepage-project-screenshot-mobile" src={`/img/${project.id}/screenshot-0.png`} alt={`${project.name} screenshot`} />}
 
 				<p>{t(`projects.${project.id}.description`)}</p>
+
+				<div className="homepage-project-link-list">
+					{project.links.map((x, i) => (
+						<a key={i} className="homepage-project-link" target="_blank" href={x.url}>
+							<img src={x.icon} alt={x.name} />
+							<span>{x.name}</span>
+						</a>
+					))}
+				</div>
 			</div>
 		</div>
 	);
