18316039401611906e002d08df0fc35a703908d5
Elanis <elanis@hotmail.com>
2024-11-17 18:28:04 +0100

    Replace Twitter link by Bluesky/Mastodon

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -10,8 +10,9 @@ export default function Contact() {
 			<h2>{t('contact.title')}</h2>
 			<p>{t('contact.text')}</p>
 			<ul>
+				<li><a href="https://bsky.app/profile/elanis.bsky.social">Bluesky</a></li>
 				<li><a href="https://discord.com/invite/c8aARey">Discord</a></li>
-				<li><a href="https://twitter.com/DysnomiaStudio">Twitter</a></li>
+				<li><a href="https://mastodon.gamedev.place/@Elanis">Mastodon</a></li>
 			</ul>
 		</div>
 	);
