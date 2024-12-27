8c78fdabdcc78486c16f219cc6087c1db14be5b0
Elanis <elanis@hotmail.com>
2024-12-27 11:47:20 +0100

    Please work
    It works locally but not on CI, it's really frustrating

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), astroI18next()]
+	integrations: [react(), astroI18next({ configPath: 'astro-i18next.config.js' })],
+	root: './'
 });
