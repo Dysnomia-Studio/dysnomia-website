1c939f03cc3c1c252c9ee40fc6902d2629c909ee
Elanis <elanis@hotmail.com>
2024-12-27 10:23:44 +0100

    Update bluesky handle

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
 			<h2>{t('contact.title')}</h2>
 			<p>{t('contact.text')}</p>
 			<ul>
-				<li><a href="https://bsky.app/profile/elanis.bsky.social">Bluesky</a></li>
+				<li><a href="https://bsky.app/profile/elanis.eu">Bluesky</a></li>
 				<li><a href="https://discord.com/invite/c8aARey">Discord</a></li>
 				<li><a href="https://mastodon.gamedev.place/@Elanis">Mastodon</a></li>
 			</ul>
